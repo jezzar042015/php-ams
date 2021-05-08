64e15ca4f547214e8a1f60fb976f5dcb3e05ba1f
Jezreel D Tan <34290328+jezzar042015@users.noreply.github.com>
2021-05-08 22:54:31 +0800

    Update read.php
    Update comments

--- api/accounts/read.php
+++ api/accounts/read.php
@@ -7,14 +7,14 @@ header("Content-Type: application/json; charset=UTF-8");
 include_once '../config/database.php';
 include_once '../objects/account.php';
 
-// instantiate database and product object
+// instantiate database object
 $database = new Database();
 $db = $database->getConnection();
   
-// initialize object
+// initialize account object
 $account = new Account($db);
   
-// query products
+// query accounts
 $stmt = $account->read();
 $num = $stmt->rowCount();
   
@@ -74,7 +74,7 @@ if($num>0){
     // set response code - 200 OK
     http_response_code(200);
   
-    // show products data in json format
+    // show accounts data in json format
     echo json_encode($accounts_arr);
 }
   
@@ -83,7 +83,7 @@ else{
     // set response code - 404 Not found
     http_response_code(404);
   
-    // tell the user no products found
+    // tell the user no accounts found
     echo json_encode(
         array("message" => "No accounts found.")
     );
