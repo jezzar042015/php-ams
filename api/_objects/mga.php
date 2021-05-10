0c0761373f8bc0f541c9b76a1dfdb7f79a6cd43a
Jezreel D Tan <34290328+jezzar042015@users.noreply.github.com>
2021-05-10 22:29:30 +0800

    Update mga.php

--- api/_objects/mga.php
+++ api/_objects/mga.php
@@ -1,3 +1,49 @@
-<?php
+<?php 
 
+class MGA {
+
+
+    private $conn;
+    private $table_name = "mgas";
+
+    //properties
+    public $mgaid;
+    public $mganame;
+    public $writingstate;
+    public $carriers;
+    public $phones;
+    public $emails;
+    public $mga_address;
+    public $mga_city;
+    public $mga_cityname;
+    public $mga_state;
+    public $mga_zip;
+    public $website;
+    public $endtFees;
+    public $notes;
+    public $created;
+    public $modified;
+
+
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    function read() {
+
+    }
+
+    function create() {
+
+    }
+
+    function update() {
+
+    }
+
+    function search() {
+        
+    }
+}
 ?>
