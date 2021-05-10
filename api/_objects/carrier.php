e9a44df8d6966473f2e75a66737879e8420a79bf
Jezreel D Tan <34290328+jezzar042015@users.noreply.github.com>
2021-05-10 22:33:02 +0800

    Update carrier.php

--- api/_objects/carrier.php
+++ api/_objects/carrier.php
@@ -1,4 +1,46 @@
-<?php
+<?php 
+
+class Carrier {
 
 
+    private $conn;
+    private $table_name = "carriers";
+
+    //properties
+    public $carrierid;
+    public $carriername;
+    public $agencycode;
+    public $ambest_rating;
+    public $phones;
+    public $emails;
+    public $carr_address;
+    public $carr_city;
+    public $carr_state;
+    public $carr_zip;
+    public $website;
+    public $notes;
+    public $created;
+    public $modified;
+
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    function read() {
+
+    }
+
+    function create() {
+
+    }
+
+    function update() {
+
+    }
+
+    function search() {
+
+    }
+}
 ?>
