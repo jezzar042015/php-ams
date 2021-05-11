5e1691b769a9d93c0d54714b6423f4b7a797d68e
Jezreel D Tan <34290328+jezzar042015@users.noreply.github.com>
2021-05-11 22:03:48 +0800

    Update contact.php

--- api/_objects/contact.php
+++ api/_objects/contact.php
@@ -1,3 +1,46 @@
 <?php
-
+class Contact {
+    
+  //database connection and table name
+  private $conn;
+  private $table_name = "contacts";
+  
+  //object properties
+  public $contactid;
+  public $firstname;
+  public $middlename;
+  public $lastname;
+  public $title;
+  public $mobile_phone;
+  public $business_phone;
+  public $email1;
+  public $email2;
+  public $company_type;
+  public $company_ref;
+  
+  public $created;
+  public $modified;
+  
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+  
+  function read() {
+  
+  }
+  
+  function create() {
+  
+  }
+  
+  function delete() {
+  
+  }  
+  
+  function search($keywords) {
+  
+  }
+  
+  
+}
 ?>
