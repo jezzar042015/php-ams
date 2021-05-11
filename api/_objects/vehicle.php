8a2711817cf04daf7b27d73944fae8838e6a035a
Jezreel D Tan <34290328+jezzar042015@users.noreply.github.com>
2021-05-11 22:45:23 +0800

    Update vehicle.php

--- api/_objects/vehicle.php
+++ api/_objects/vehicle.php
@@ -6,6 +6,29 @@ class Vehicle {
   
   //object properties
   public $vehicleID;
+  public $vin;
+  public $year;
+  public $makeid;
+  public $makename;
+  public $typeid;
+  public $typename;
+  public $model;
+  public $unit_number;
+  public $pdvalue;
+  public $driverid;
+  public $drivername;
+  public $lienholder;
+  public $lienholdername;
+  public $accountid;
+  public $legalname;
+  public $created;
+  public $modified;
   
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+  
+  function read() {
+  }
 }
 ?>
