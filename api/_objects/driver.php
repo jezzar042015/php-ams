5d881ce329044d65042ae667eea2821300d32fb4
Jezreel D Tan <34290328+jezzar042015@users.noreply.github.com>
2021-05-11 22:38:11 +0800

    Update driver.php

--- api/_objects/driver.php
+++ api/_objects/driver.php
@@ -11,14 +11,15 @@ class Driver {
   public $middlename;
   public $lastname;
   public $hiredate;
-  public $terminatedate;
+  public $terminationdate;
   public $dob;
   public $cdl_state;
   public $cdl_number;
-  public $years_licensed;
+  public $year_licensed;
   public $phone;
   public $email;
   public $accountid;
+  public $legalname;
   public $created;
   public $modified;
   public $policies;
@@ -28,6 +29,84 @@ class Driver {
     $this->conn = $db;
   }
   
+  function read() {
+  
+  }
+  
+  function create() {
+      //creating query
+      $query = 
+        "INSERT INTO drivers 
+        SET
+          firstname=:firstname,
+          middlename=:middlename,
+          lastname=:lastname,
+          hiredate=:hiredate,
+          terminationdate=:terminationdate,
+          dob=:dob,
+          cdl_state=:cdl_state,
+          cdl_number=:cdl_number,
+          year_licensed=:year_licensed,
+          phone=:phone;
+          email=:email;
+          accountid=:accountid,
+          created=:created
+        ";
+    
+      //prepare query
+      $stmt = $this->conn->prepare($query);
+    
+      //sanitize data
+      $this->firstname = htmlspecialchars(strip_tags($this->firstname));
+      $this->middlename = htmlspecialchars(strip_tags($this->middlename));
+      $this->lastname = htmlspecialchars(strip_tags($this->lastname));
+      $this->hiredate = htmlspecialchars(strip_tags($this->hiredate));
+      $this->terminationdate = htmlspecialchars(strip_tags($this->terminationdate));
+      $this->dob = htmlspecialchars(strip_tags($this->dob));
+      $this->cdl_state = htmlspecialchars(strip_tags($this->cdl_state));
+      $this->cdl_number = htmlspecialchars(strip_tags($this->cdl_number));
+      $this->year_licensed = htmlspecialchars(strip_tags($this->year_licensed));
+      $this->phone = htmlspecialchars(strip_tags($this->phone));
+      $this->email = htmlspecialchars(strip_tags($this->email));
+      $this->accountid = htmlspecialchars(strip_tags($this->accountid));
+    
+      //binding values
+      $stmt->bindParam(":firstname",$this->firstname);
+      $stmt->bindParam(":middlename",$this->middlename);
+      $stmt->bindParam(":lastname",$this->lastname);
+      $stmt->bindParam(":hiredate",$this->hiredate);
+      $stmt->bindParam(":terminationdate",$this->terminationdate);
+      $stmt->bindParam(":dob",$this->dob);
+      $stmt->bindParam(":cdl_state",$this->cdl_state);
+      $stmt->bindParam(":cdl_number",$this->cdl_number);
+      $stmt->bindParam(":year_licensed",$this->year_licensed);
+      $stmt->bindParam(":phone",$this->phone);
+      $stmt->bindParam(":email",$this->email);
+      $stmt->bindParam(":accountid",$this->accountid);
+      $stmt->bindParam(":created",$this->created);
+    
+      //execute query
+      if ($stmt->execute()) {
+          return array(
+              "message"=>"Driver was added successfully",
+              "drivername"=>$this->firstname . " " . $this->lastname,
+              "driverid"=>$this->driverid,
+              "status"=>"200"
+          )
+      }
+      else {
+          return array(
+              "message"=>"Driver insert was failed",
+              "drivername"=>$this->firstname . " " . $this->lastname,
+              "status"=>"400"
+        
+      }  
+  }
+            
+  
+  function search() {
+  
+  }
   
 }
 
